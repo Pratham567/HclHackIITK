1665c98b1acd5e9251e187ff9d28f25610c744fb
Pratham Gupta <prathamgupta567@gmail.com>
2020-07-25 21:50:22 +0530

    Fix text file issue and Code Cleanup

--- README.md
+++ README.md
@@ -1,28 +1,37 @@
 # HclHackIITK
-A cybersecurity hackathon
-There are two types of programming questions, one is advanced coding and the other one is related to ML Classification.
+A cybersecurity hackathon.
 
+There were two types of programming questions, one was related to advanced coding and the other one was related to ML Classification.
 
-You are required to write a Python function to read a file from the given 
-. The file contains text lines and your function has to read each line and detect if the line is a palindrome along with count the number of characters.
+## Question 1 (coding: Palindromes)
+
+You are required to write a Python function to read a file from the given. 
+The file contains text lines and your function has to read each line and detect if the line is a palindrome along with count the number of characters.
 
 If the line is a palindrome, then record the line number of this line in the file. The line number starts from 1. Not all lines are necessarily palindromes. Finally, count all the number of palindromes.
 Your program needs to return a dictionary with the line number of a palindrome and number of characters in the palindrome (excluding blank spaces) along with a string representing the status of reading the file. 
-If the file has no palindrome, the dictionary returned should contain 
+If the file has no palindrome, the dictionary returned should contain {0:0}
 If the URL is correct and the file is a txt file, the status message should be "file ok".
 If the URL is correct but the file is not a txt file, the status message should be "file is not a text file".
 If the URL is not a valid URL, the status message should be "the URL is incorrect".
 Input Specification:
- The string value representing the URL of the file.
- Output Specification:
- Return an object of the "Result" class. The object should have:
- a dictionary with the key as the line number and value as the count of the number of characters
- a string representing the status message
+The string value representing the URL of the file.
+Output Specification:
+Return an object of the "Result" class. The object should have:
+a dictionary with the key as the line number and value as the count of the number of characters
+a string representing the status message
 
- Example
+Example Input:
+
   https://mettl-arq.s3-ap-southeast-1.amazonaws.com/questions/iit-kanpur/cyber-security-hackathon/round1/problem1/defaulttestcase.txt
 
-  Output:
+Output:
 
-   {2:4, 5:7, 6:30, 8:30, 9:9, 12:5, 14:11}
-    "file ok"
+output1: {2:4, 5:7, 6:30, 8:30, 9:9, 12:5, 14:11}
+
+output2: "file ok"
+
+Solution:
+The Solution is given in the location /coding/q1.py
+
+## Question 2 (ML: )
--- coding/q1.py
+++ coding/q1.py
@@ -1,10 +1,5 @@
 import requests
-import copy
-
-class Result:
-    def __init__(self):
-        self.out1 = dict()
-        self.out2 = ''
+import os
 
 def check_palindrome(strng):
     # reverse the string
@@ -12,10 +7,8 @@ def check_palindrome(strng):
 
     # check if the string is equal to its reverse
     if list(strng) == list(rev_str):
-        # print("The string is a palindrome.")
         return True
     else:
-        # print("The string is not a palindrome.")
         return False
 
 def probe_url(url):
@@ -26,7 +19,7 @@ def validate_response(response):
         return True
     return False
 
-def check_if_text(response):
+def check_if_there_is_response(response):
     if response.text:
         return True
     return False
@@ -34,7 +27,7 @@ def check_if_text(response):
 def parse_response(response):
     if not validate_response(response):
         return "the URL is incorrect", True
-    if not check_if_text(response):
+    if not check_if_there_is_response(response):
         return "file is not a text file", True
     return response.text, False
 
@@ -43,41 +36,40 @@ def process_line(line):
     line = line.replace(" ", "").lower()
     return line
 
+def check_text_file(response):
+    # check if the response received is of type text/plain
+
+    if response.headers.get('content-type') == 'text/plain':
+        return True
+    return False
+
 def main(url):
     output1 = dict()
     output2 = ''
     atleastOnePalindrome = False
-    ret = probe_url(url)
-    msg, err = parse_response(ret)
-    if err:
-        output2 = msg
-        output1 = {0:0}
-    else:
-        # The validations are passed
-        # now check palindrome
-        i = 0
-        # print("FLAG: check msg")
-        # print(msg)
-        for lin in msg.splitlines():
-            line = copy.deepcopy(lin)
-            i += 1
-            # print("FLAG: check line Before")
-            # print(line)
-            line = process_line(line)
-            # print("FLAG: check line")
-            # print(line)
-            # print(len(line))
-            # process line
-            if line and check_palindrome(line):
-                # print("line {} is palindrome".format(i))
-                output1[i] = len(line)
-                atleastOnePalindrome = True
-        if not atleastOnePalindrome:
+    response = probe_url(url)
+    if check_text_file(response):
+        msg, err = parse_response(response)
+        if err:
+            output2 = msg
             output1 = {0:0}
-        output2 = 'file ok'
-    
-    assert check_palindrome('do od')
-    # assert not check_palindrome('do od')
+        else:
+            # The validations are passed
+            # now check palindrome
+            i = 0
+            for line in msg.splitlines():
+                i += 1
+                line = process_line(line)
+                if line and check_palindrome(line):
+                    output1[i] = len(line)
+                    atleastOnePalindrome = True
+            if not atleastOnePalindrome:
+                output1 = {0:0}
+            output2 = 'file ok'
+    else:
+        output1 = {0:0}
+        output2 = "file is not a text file"
+        # the url is not text file
 
     print("CHEKING THE OUTPUT")
     print("OUTPUT1: " + str(output1))
@@ -86,12 +78,5 @@ def main(url):
 
 if __name__ == "__main__":
     url = 'https://mettl-arq.s3-ap-southeast-1.amazonaws.com/questions/iit-kanpur/cyber-security-hackathon/round1/problem1/defaulttestcase.txt'
+    # url = 'https://sjmulder.nl/en/textonly.html'
     main(url)
-
-    # print(process_line("Do od"))
-
-
-# Test cases
-# assert check_palindrome('aba')
-# assert not check_palindrome ("aaab")
-# assert check_palindrome ("aaab")
